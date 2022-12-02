825b5cd07b0606c04f69804cdcd41089649c4846
strongcookdas <goodjav141592@gmail.com>
2022-12-02 09:33:38 +0900

    build : 로그인 api 구현 시작

---
+++ study_springboot_login2/src/main/java/com/example/study_springboot_login2/StudySpringbootLogin2Application.java
@@ -0,0 +1,13 @@
+package com.example.study_springboot_login2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootLogin2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringbootLogin2Application.class, args);
+    }
+
+}
