2ea749e4416f65f52f6d6b02ff447ff92e76f57d
strongcookdas <goodjav141592@gmail.com>
2022-12-07 11:52:50 +0900

    build : start springboot visit history

---
+++ springboot_visithistory/README.md
@@ -0,0 +1,4 @@
+# SpringBoot 방문기록 구현
+
+## ERD 다이어그램
+![img.png](image/img.png)
---
+++ springboot_visithistory/src/main/java/com/example/springboot_visithistory/SpringbootVisithistoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_visithistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVisithistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVisithistoryApplication.class, args);
+    }
+
+}
